a1f81c41935834f270a9badcf1aa75db8d48b8fa
omshii <omshisamal72@gmail.com>
2021-01-16 15:39:18 -0500

    cleaned repo

--- parameter_selection/nfs_ode.m
+++ parameter_selection/nfs_ode.m
@@ -1,6 +1,6 @@
 %NFS Model
 
-function dydt = nfs_ode(t, y, k1, k2, k3, K3, k4, K4)
+function dydt = nfs_ode(t, y, params)
 
 A = y(1);
 B = y(2);
@@ -11,6 +11,6 @@ else
     I = 0.2;
 end
 
-dA = k1*I*(1-A)-k2*A*B;
-dB = k3*A*(1-B)/(K3+1-B)-k4*B/(K4+B);
+dA = params(1)*I*(1-A)-params(2)*A*B;
+dB = params(3)*A*(1-B)/(params(4)+1-B)-params(5)*B/(params(6)+B);
 dydt = [dA; dB];
